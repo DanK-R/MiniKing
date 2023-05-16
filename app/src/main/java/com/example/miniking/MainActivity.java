8c722a8076c3f5bd16d046e10a020799db942e84
Danon <danonkr@gmail.com>
2023-05-16 19:24:05 -0230

    Hid Action Bar and Status Bar

--- app/src/main/java/com/example/miniking/MainActivity.java
+++ app/src/main/java/com/example/miniking/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.miniking;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
+
+
 }
